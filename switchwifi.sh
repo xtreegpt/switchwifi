35c23d53deae5e6b145b09f1409ebe8801d7cf05
Greg Treantos <gtreanto@gmail.com>
2023-04-09 12:00:39 -0400

    Update switchwifi.sh

--- switchwifi.sh
+++ switchwifi.sh
@@ -2,10 +2,11 @@
 COUNT=0
 NETS=$( wpa_cli -i wlan0 list_networks )
 
-
+#################### Print Header for selection #####################
 printf "\n%s\t %-s\t\t %-9s %s\n" NETWORK_ID ESSID BSSID FLAGS
 printf "%s\t %-s\t\t %-9s %s\n" ---------- ----- ----- -----
 
+#################### Use sed to skip first line and loop through the rest of $NETS ##########################
 echo "$NETS" |sed '1d' | (while read i
 do
         echo "$i" | awk '{printf "%5s\t\t %-16s %-6s %-s\n", $1, $2, $3, $4}'
